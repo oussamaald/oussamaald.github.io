f993e6e0b357a3f008a2ec2ddcc52c7ac646945c
oussamaald <90107105+oussamaald@users.noreply.github.com>
2024-12-03 12:51:18 +0100

    Update metamask.js

--- metamask.js
+++ metamask.js
@@ -6,7 +6,7 @@ metamaskButton.addEventListener('click', () => {
     console.log("MetaMask Login Button Clicked");
 
     // الرابط الذي يفتح تطبيق MetaMask
-    const deepLink = "metamask://dapp/https://oussamaald.github.io/"; // تعديل الرابط ليتوافق مع GitHub Pages
+    const deepLink = "https://oussamaald.github.io/"; // تعديل الرابط ليتوافق مع GitHub Pages
     window.location.href = deepLink;
 });
 
